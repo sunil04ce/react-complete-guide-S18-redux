3c41b329e6e7c1d8fce88496ac301d649f865b08
sunil parmar <sunil04ce@gmail.com>
2023-02-27 07:58:43 +0530

    multiple slices, splitted slices in seperate files

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,11 +1,11 @@
 import { useSelector, useDispatch } from "react-redux";
 import classes from "./Counter.module.css";
-import { counterActions } from "../store/index";
+import { counterActions } from "../store/counter-slice";
 
 const Counter = () => {
   const dispatch = useDispatch();
-  const counter = useSelector((state) => state.counter);
-  const showCounter = useSelector((state) => state.showCounter);
+  const counter = useSelector((state) => state.counter.counter);
+  const showCounter = useSelector((state) => state.counter.showCounter);
 
   const incrementHandler = () => {
     dispatch(counterActions.increment());
--- src/store/index.js
+++ src/store/index.js
@@ -1,26 +1,7 @@
 // import { createStore } from 'redux';
-import { createSlice, configureStore } from '@reduxjs/toolkit';
-
-const initialState = { counter: 0, showCounter: true };
-
-const counterSlice = createSlice({
-    name: 'counter',
-    initialState,
-    reducers: {
-        increment(state) {
-            state.counter++;
-        },
-        decrement(state) {
-            state.counter--;
-        },
-        increase(state, action) {
-            state.counter = state.counter + action.payload;
-        },
-        toggleCounter(state) {
-            state.showCounter = !state.showCounter;
-        },
-    }
-});
+import { configureStore } from '@reduxjs/toolkit';
+import counterReducer from './counter-slice';
+import authReducer from './auth-slice';
 
 // const counterReducer = (state = initialState, action) => {
 //     if (action.type === 'increment') {
@@ -58,8 +39,7 @@ const counterSlice = createSlice({
 // const store = configureStore(counterSlice.reducer);
 
 const store = configureStore({
-    reducer: counterSlice.reducer
+    reducer: { counter: counterReducer, auth: authReducer },
 });
 
-export const counterActions = counterSlice.actions
 export default store;
